6d01690d575960071ff94b2524c8e171328ec706
yadwinder <yadwinder.banwait@gmail.com>
2025-11-11 17:59:54 -0500

    Code to get book detail by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,8 +19,13 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  let isbn = req.params.isbn;
+  if(!books[isbn]) {
+    return res.status(404).json({message: `Could not find the book with ISBN: ${isbn}`});
+  }  
+
+  return res.send(books[isbn])
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
